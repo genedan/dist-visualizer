436f848e413b9d4f55c562d8c163c48676134664
genedan <genedan@gmail.com>
2015-05-29 17:03:10 -0500

    added beta and ecdf

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(tweedie)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
+  # The expression is
   # wrapped in a call to renderPlot to indicate that:
   #
   #  1) It is "reactive" and therefore should re-execute automatically
@@ -12,10 +12,23 @@ shinyServer(function(input, output) {
   #  2) Its output type is a plot
   
   output$distPlot <- renderPlot({
-    x    <-   rtweedie(input$observations,input$power,input$mu,input$phi)
+    par(mfrow=c(2,1))
+    # generates dataset based on selected distribution
+    if(input$Distribution == "Tweedie")
+    {
+      x    <-   rtweedie(input$observations,input$power,input$mu,input$phi)
+    } else {
+      x    <-   rbeta(input$observations,input$alpha,input$beta)
+    }
+    
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
+    # calculate the empirical cumulative distribution
+    P = ecdf(x)
+    P(0.0)    
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'lightblue', border = 'white',main="Histogram of Tweedie Distribution")
+    title = paste("Histogram of ",input$Distribution," Distribution",sep='')
+    p1 = hist(x, breaks = bins, col = 'lightblue', border = 'white',main=title)
+    p2 = plot(P, main = "Empirical Cumulative Distribution")
   })
 })
--- ui.R
+++ ui.R
@@ -9,43 +9,65 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
+      selectInput("Distribution","Distribution",c("Beta","Tweedie"),selected="Tweedie"),
       sliderInput("bins",
                   "Number of bins:",
                   min = 1,
                   max = 50,
                   value = 30)
-    ,
-    sliderInput("observations",
-                "Number of observations:",
-                min = 1,
-                max = 20000,
-                value = 10000)
-    ,
-    sliderInput("power",
-                "Power ξ:",
-                min = 1,
-                max = 2,
-                value = 1.5,
-                step=.1)
-    ,
-    sliderInput("mu",
-                "Mean μ:",
-                min = 0,
-                max = 1000,
-                value = 1,
-                step = .1)
-    ,
-    sliderInput("phi",
-                "Dispersion φ:",
-                min = 0,
-                max = 5,
-                value = 1,
-                step = .1)
-  ),
+      ,
+      sliderInput("observations",
+                  "Number of observations:",
+                  min = 1,
+                  max = 20000,
+                  value = 10000),
+      
+      # Panels to be displayed for Beta
+      conditionalPanel(
+        condition = "input.Distribution == 'Beta'",
+        sliderInput("alpha",
+                    "Shape Parameter α:",
+                    min = 0,
+                    max = 5,
+                    value = 2.5,
+                    step = .1)
+        ,
+        sliderInput("beta",
+                    "Shape Parameter β:",
+                    min = 0,
+                    max = 5,
+                    value = 2.5,
+                    step = .1)
+        ),
+      
+      # Panels to be displayed for Tweedie
+      conditionalPanel(
+        condition = "input.Distribution == 'Tweedie'",
+        sliderInput("power",
+                    "Power ξ:",
+                    min = 1,
+                    max = 2,
+                    value = 1.5,
+                    step=.1)
+        ,
+        sliderInput("mu",
+                    "Mean μ:",
+                    min = 0,
+                    max = 1000,
+                    value = 1,
+                    step = .1)
+        ,
+        sliderInput("phi",
+                    "Dispersion φ:",
+                    min = 0,
+                    max = 5,
+                    value = 1,
+                    step = .1)
+  )),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("distPlot",width = "600px", height="800px")
     )
   )
 ))
